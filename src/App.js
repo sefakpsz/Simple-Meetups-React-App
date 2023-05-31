9bf6aa3e63e89e47b53f2ad3644e4347e0b06c6f
sefa kapısız <sefa.kapisiz@coino.com>
2023-05-31 09:02:41 +0300

    components of meetupItem and meetupList and adding layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups'
 import FavoritesPage from './pages/Favorites'
 import NewMeetupPage from './pages/NewMeetup'
+import Layout from './components/layout/Layout'
 
 const App = () => {
   return (
-    <div >
+    <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetupPage />} />
         <Route path='/favorites' element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   )
 }
 
