442290ac0f8485cc58331b4abcce272c6ba5b4f8
sefa kapısız <sefa.kapisiz@coino.com>
2023-06-01 09:03:11 +0300

    requested post to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,24 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm"
+
 const NewMeetup = () => {
-    return <div>New Meetup</div>
+    const addMeetupHandler = (meetupData) => {
+        fetch('https://react-getting-started-6fd29-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+    }
+
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </section>
+    )
 }
 
 export default NewMeetup
